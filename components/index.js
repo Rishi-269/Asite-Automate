3413a0c2892dec61ad8cbfe72fdc34d5f49fe058
Rishi-269 <rishiprakash269@gmail.com>
2024-07-19 21:42:30 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 import login from './login.js'
 import system_task_CLT from "./system_task/system_task_CLT.js"
-// import trigger from "./trigger/trigger.js"
+import trigger_CLT from "./trigger/trigger_CLT.js"
 import app_builder_CLT from "./app_builder/app_builder_CLT.js"
 
-export {login, system_task_CLT, app_builder_CLT}
+export {login, system_task_CLT, app_builder_CLT, trigger_CLT}
